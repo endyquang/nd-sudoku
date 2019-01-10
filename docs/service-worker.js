c1285698b2b358f39d9606c2c1f7776b6feb398e
NDQ <endyquang@gmail.com>
2019-01-10 22:15:41 +0700

    fix win

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["css/app.641b1726.css","7b2c710fdd4106948bfddf1e765a24b4"],["favicon.ico","657800290452f3daaa47d1145421f0d8"],["github.png","f87561b8bb354ef83b09a66e54f70e08"],["index.html","e08de407f8cde244239b449df4e1b843"],["js/app.84570376.js","74678e46bd19387a9078e8421e9a4eb1"],["js/chunk-vendors.4d2394c0.js","75e07f292fa274c5a2bd4b68244ef68a"],["manifest.json","b61891753ebb6a4c8f5cf571009bfcf1"]];
+var precacheConfig = [["css/app.fbbd8f00.css","b70a819121cdde9e7ab33050469bf3fe"],["favicon.ico","657800290452f3daaa47d1145421f0d8"],["github.png","f87561b8bb354ef83b09a66e54f70e08"],["index.html","a1fccd52ba9f1e09b464f1f8ec15f599"],["js/app.23777143.js","1c21df458e60038a099ba96144681c5b"],["js/chunk-vendors.4d2394c0.js","75e07f292fa274c5a2bd4b68244ef68a"],["manifest.json","b61891753ebb6a4c8f5cf571009bfcf1"]];
 var cacheName = 'sw-precache-v3-blabla-' + (self.registration ? self.registration.scope : '');
 
 
